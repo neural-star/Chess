25077bdd98b17da1c4492adb9f23e971d8e80f76
NEURAL-STAR <161698386+neural-star@users.noreply.github.com>
2025-06-07 12:37:47 +0900

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmake
+from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chess.db")
